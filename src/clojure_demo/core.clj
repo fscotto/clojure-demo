3eb002e0712ebb1d95e388be94fb36a4e65199d4
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2020-09-20 10:30:09 +0200

    Examples and exercises chapter 3 "Clojure for the Brave and True"

--- src/clojure_demo/core.clj
+++ src/clojure_demo/core.clj
@@ -1,7 +1,82 @@
 (ns clojure-demo.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;========================= CHAPTER 3 ====================================
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  [part]
+  {:name (str/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce #(into %1 (set [%2 (matching-part %2)]))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-parts-size-sum (reduce + (map :size sym-parts))
+        target (rand body-parts-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(defn recursive-printer
+  ([]
+   (recursive-printer 0))
+  ([iteration]
+  (println "Iteration " iteration)
+  (if (> iteration 3)
+    (println "Goodbye!")
+    (recursive-printer (inc iteration)))))
+
+(defn add-100
+  "This function add 100 to x"
+  [x]
+  (+ x 100))
+
+(defn dec-maker
+  "Return new function that decrement n to x"
+  [n]
+  (fn [x] (- x n)))
+
+(defn mapset
+  "This function work like map, but return a set collection"
+  [f s]
+  (into (sorted-set) (map f s)))
